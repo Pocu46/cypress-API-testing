f63c5c40ce300cc2799552343d2be0459e21f231
pocu46 <andrew.pulyaev@gmail.com>
2022-08-10 18:03:10 +0300

    cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  projectId: "98emj7",
   e2e: {
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
